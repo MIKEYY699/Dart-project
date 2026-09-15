65b8b0084af50f7049a9f6f4c57bd313d52ca23c
MIKEYY699 <khuzaimare@gmail.com>
2026-09-15 15:55:06 +0500

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const new({
-    super.key,
-  });
+  const new({super.key});
+
+  // new comment
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home:Scaffold(backgroundColor: Colors.limeAccent));
+    return MaterialApp(home: Scaffold(backgroundColor: Colors.limeAccent));
   }
 }
