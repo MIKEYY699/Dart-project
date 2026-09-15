1e95cc973b270c82bffcf1b878157b8328366648
MIKEYY699 <khuzaimare@gmail.com>
2026-09-15 16:15:58 +0500

    Add HomeView file

---
+++ lib/homeView.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+class HomeView extends StatelessWidget{
+    HomeView({super.key});
+    @override
+
+    Widget build(context){
+        return Scaffold(
+          backgroundColor: Colors.white70,
+          body: ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>))
+          }, child: Text("data")),
+          
+          
+          
+          
+          );
+            
+            
+        
+
+
+
+    }
+}
